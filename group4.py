f2766eb2577455199db26ae2806a94f2cce0d580
Audric <tph4d22@gmail.com>
2024-03-04 13:19:30 +0800

    Tasks completed

--- group4.py
+++ group4.py
@@ -3,7 +3,7 @@ import random
 text = './news.txt'
 f = open(text, "r")
 print(f.read())
-
+txt = f.read().replace('\n',' ')
 
 def hammer_task_0():
     '''example function'''
@@ -13,16 +13,45 @@ def hammer_task_0():
     return result
   
 def memberOne():
-    pass
-  
-def memberTwo():
-    pass
+    #Task 1
+    count=0
+    for i in txt:
+        if i in ('a','e','i','o','u'):
+        # if the word in the news match with any word in the turple, add the number of count by one.
+            count+=1
+    return(count)
+
+def memberTwo(n):
+    #Task 2
+    ans = ""
+    for i in range(len(txt)):
+        ch = txt[i]
+        if ch==" ":
+            ans+=" "
+        elif (ch.isupper()):
+            # use ASCii code to find the word is encrypt to [A:65-Z:90]
+            ans += chr((ord(ch) + n-65) % 26 + 65)
+        else:
+            # use ASCii code to find the word is encrypt to [a:97-z:122]
+            ans += chr((ord(ch) + n-97) % 26 + 97)
+    return ans
   
 def memberThree():
-    pass
+    #Task 3
+    #slice the word backward 
+    result = txt[::-1]
+    return(result)
   
 def memberFour():
-    pass
+    #Task 4
+    #separate the word by space
+    result = txt.split(' ')
+    temp =[]
+    for i in result:
+        #slice the word backward
+        temp.append(i[::-1])
+    #combine the list in to a single line
+    return(' '.join(temp))
 
 
 if __name__ == "__main__":
